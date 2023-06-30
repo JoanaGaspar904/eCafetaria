d57e006a167344fd0e657dda57b83584de04f4b8
Joana Gaspar <jfgaspar96@gmail.com>
2023-06-30 09:02:16 +0100

    Initial commit

---
+++ src/main/java/com/example/eCafetaria/ECafetariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.eCafetaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECafetariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECafetariaApplication.class, args);
+	}
+
+}
