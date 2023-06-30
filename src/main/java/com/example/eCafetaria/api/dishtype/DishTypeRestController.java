70b125c7686f8358a44a9ee64b091452d0263e70
veronica <veronicasousacr@gmail.com>
2023-06-30 15:35:13 +0100

    Create findAllDishType, in DishTypeRepository, DishTypeRestController, FindDishTypeController

--- src/main/java/com/example/eCafetaria/api/dishtype/DishTypeRestController.java
+++ src/main/java/com/example/eCafetaria/api/dishtype/DishTypeRestController.java
@@ -1,4 +1,17 @@
 package com.example.eCafetaria.api.dishtype;
 
+import com.example.eCafetaria.application.dishtype.FindDishTypeController;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+
+
 public class DishTypeRestController {
+    @Autowired
+    FindDishTypeController findDishTypeController;
+
+    public List<CreateOrUpdateDishTypeDTO> searchAllDishType() {
+        List<CreateOrUpdateDishTypeDTO> dishTypeDTOList = new ArrayList<>();
+        dishTypeDTOList = findDishTypeController.findAllDishType();
+        return dishTypeDTOList;
+    }
 }
