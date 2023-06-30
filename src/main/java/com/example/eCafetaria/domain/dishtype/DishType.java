2c5f0efc0a9835555a91e8e73a1e2b658e3b31ef
Maria Joao <mariajoaotg_13@hotmail.com>
2023-06-30 15:27:27 +0100

    Class DishType: Acronym rule of maximum characters created within a set method.

--- src/main/java/com/example/eCafetaria/domain/dishtype/DishType.java
+++ src/main/java/com/example/eCafetaria/domain/dishtype/DishType.java
@@ -1,4 +1,17 @@
 package com.example.eCafetaria.domain.dishtype;
 
 public class DishType {
+    private String acronym;
+    private DishTypeDesignation designation;
+
+    public DishType(String acronym, DishTypeDesignation designation) {
+        setAcronym(acronym);
+        this.designation = designation;
+    }
+
+    public void setAcronym(String acronym) {
+        if(acronym.length() > 3 || acronym.length() < 1)
+            throw new StringIndexOutOfBoundsException();
+        this.acronym = acronym;
+    }
 }
