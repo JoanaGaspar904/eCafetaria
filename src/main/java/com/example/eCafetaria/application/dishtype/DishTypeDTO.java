fe8ff757765613832daec0c4a6c74bb8a213cf08
veronica <veronicasousacr@gmail.com>
2023-07-03 14:26:45 +0100

    Change Acronym and Designation to final.

--- src/main/java/com/example/eCafetaria/application/dishtype/DishTypeDTO.java
+++ src/main/java/com/example/eCafetaria/application/dishtype/DishTypeDTO.java
@@ -1,6 +1,5 @@
 package com.example.eCafetaria.application.dishtype;
 
-import com.example.eCafetaria.domain.dishtype.Designation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
--- src/main/java/com/example/eCafetaria/domain/dishtype/Acronym.java
+++ src/main/java/com/example/eCafetaria/domain/dishtype/Acronym.java
@@ -1,21 +1,17 @@
 package com.example.eCafetaria.domain.dishtype;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Id;
 import java.io.Serializable;
 
 @Embeddable
 public class Acronym implements Serializable {
-    private String acronym;
+    private final String acronym;
 
     protected Acronym() {
+        acronym = null;
     }
 
     public Acronym(String acronym) {
-        updateAcronym(acronym);
-    }
-
-    private void updateAcronym(String acronym) {
         this.acronym = acronym;
     }
 
